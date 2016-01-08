2296f5167cbeb2c57089cde3e4928d55131f5bc9
Hiltjo Posthuma <hiltjo@codemadness.org>
2016-01-08 22:17:54 +0100

    example.sh: fix: remove error mode, would stop if a dir was invalid
    mkdir -p makes the directory always, so no need to check.

--- example.sh
+++ example.sh
@@ -12,8 +12,6 @@
 # - mkdir -p htmldir && cd htmldir
 # - sh example.sh
 
-set -e
-
 reposdir="/var/www/domains/git.codemadness.nl/home/src"
 curdir=$(pwd)
 
@@ -34,23 +32,21 @@ done | sort -n -k 1 | cut -f 2- -d ' ' | xargs stagit-index | \
 	sed 's@<td>Last commit</td>@<td><a href="index-time.html">Last commit</a></td>@g' | \
 	sed 's@<td>Name</td>@<td><a href="index.html">Name</a></td>@g' > "${curdir}/index-time.html"
 
-cd "${reposdir}"
-
 # make files per repo.
+cd "${reposdir}"
 find . -maxdepth 1 -type d | grep -v "^.$" | sort | while read -r dir; do
 	d=$(basename "${dir}")
 	printf "%s..." "${d}"
-	cd "${curdir}"
 
-	test -d "${d}" || mkdir -p "${d}"
-	cd "${d}"
+	mkdir -p "${curdir}/${d}"
+	cd "${curdir}/${d}"
 	stagit "${reposdir}/${d}"
-
-	printf " done\n"
 
 	# symlinks
 	ln -sf log.html index.html
 	ln -sf ../style.css style.css
 	ln -sf ../logo.png logo.png
 	ln -sf ../favicon.png favicon.png
+
+	printf " done\n"
 done
