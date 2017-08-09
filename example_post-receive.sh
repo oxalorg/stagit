8787f495e09fd3857fd4bbbb7c32784f86787df6
Hiltjo Posthuma <hiltjo@codemadness.org>
2017-08-09 18:52:53 +0200

    ignore object 0000000000000000000000000000000000000000
    remote: fatal: bad object 0000000000000000000000000000000000000000
    
    this can happen on initial repos or when pushing tags etc.

--- example_post-receive.sh
+++ example_post-receive.sh
@@ -32,6 +32,8 @@ cd "${dir}" || exit 1
 # detect git push -f
 force=0
 while read -r old new ref; do
+	test "${old}" = "0000000000000000000000000000000000000000" && continue
+
 	hasrevs=$(git rev-list "${old}" "^${new}" | sed 1q)
 	if test -n "${hasrevs}"; then
 		force=1
